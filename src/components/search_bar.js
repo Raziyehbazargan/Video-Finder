0e694450baa6c2f0068c696a45594a89fd99c58d
raziyeh bazargan <r.bazargan@gmail.com>
2017-01-22 21:53:44 -0800

    add event on input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+//const Component = React.Component;
 
-const SearchBar = () => {
-  return <input />
-};
+// functional base component
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+// class base component
+class SearchBar extends Component{
+  render() {
+    return <input onChange={this.onInputChange}/>
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+}
 
 export default SearchBar;
