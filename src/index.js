5c35afe8b2747485523944f25d423341a1a02e30
raziyeh bazargan <r.bazargan@gmail.com>
2017-01-22 21:35:32 -0800

    create search_br component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = process.env.API_KEY;
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+     <div>
+        <SearchBar />
+     </div>
+  );
 }
 
 //Take this component's generetaed HTML and put it in the page. ( DOM )
-
 ReactDOM.render(<App />, document.querySelector('.container'));
