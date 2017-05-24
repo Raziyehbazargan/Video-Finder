a82043f50d1f0c0d9a9c7af9647206dc16350d5f
raziyeh bazargan <r.bazargan@gmail.com>
2017-05-23 21:50:37 -0700

    created searchbar component succeddfully

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+//because we are using JSX we need to import React Module
+const SearchBar = () => {
+  return <input />   // <-- JSX
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
 // 1- Create a new component. This component should produce some HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
   // <div>Hi!</div>: This is JSX, it's looks like HTMl but it's JavaScript
   // the main reason of using JSX is to make our component cleaner
   // for example this line in JSX is equal as below code in vanilla JavaScript
@@ -13,7 +21,7 @@ const App = () => {
     null
   )
   */
-}
+
 
 
 // 2- Take this component's generated HTML and put it on the page (in the DOM)
