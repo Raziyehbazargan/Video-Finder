3d5aeb13da3b00b2ba5b1759247df3187f1ae6b1
raziyeh bazargan <r.bazargan@gmail.com>
2017-05-23 22:06:21 -0700

    add onInputChange method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 //because we are using JSX we need to import React Module
 
-/*const SearchBar = () => {
+/* a function base component:
+const SearchBar = () => {
   return <input />;   // <-- JSX
-};*/
+};
+*/
 
 // Refactor from a function base component (above) to a class base component using ES6
 // Every class base component has a render() method
-class SearchBar extends React.component {
+// in a Class base component we need to use JSX otherwise it returns error
+
+/* a class base component */
+class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={this.onInputChange}/>;
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
