de8130913cea44d87cc9e2eb1f5f9f583d21fa2d
raziyeh bazargan <r.bazargan@gmail.com>
2017-05-23 22:58:33 -0700

    start working with api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-// 1- Create a new component. This component should produce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+const API_KEY='AIzaSyAqp4OmEviFz8RWiKzT05v85-46FkxdfZE'
+
+// 1- Create a new class base component. This component should produce some HTML.
+class App extends Component {
+  constructor(probs) {
+    super(probs);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+      console.log(data);
+    })
+  }
+  
+  render() {
+    (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 // <div>Hi!</div>: This is JSX, it's looks like HTMl but it's JavaScript
 // the main reason of using JSX is to make our component cleaner
