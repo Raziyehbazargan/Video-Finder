edff2f393651551a2054a9616a379e65f52cacfb
raziyeh bazargan <r.bazargan@gmail.com>
2017-05-23 21:01:43 -0700

    add app component

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// 1- Create a new component. This component should produce some HTML.
+const App = function() {
+  return <div>Hi!</div>;
+  // <div>Hi!</div>: This is JSX, it's looks like HTMl but it's JavaScript
+  // the main reason of using JSX is to make our component cleaner
+  // for example this line in JSX is equal as below code in vanilla JavaScript
+  /*
+   return React.createElement(
+    "div",
+    "",
+    null
+  )
+  */
+}
+
+
+// 2- Take this component's generated HTML and put it on the page (in the DOM)
+// ReactDom use to work with DOM
+ReactDom.render(<App/>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: [
-    './src/components/app.js'
+    './src/index.js'
   ],
   output: {
     path: __dirname,
