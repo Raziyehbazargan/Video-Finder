893cb53cc8f677c4afed557ec94bf3f1088d3a30
raziyeh bazargan <r.bazargan@gmail.com>
2017-05-23 22:28:14 -0700

    state and setState method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,11 +10,22 @@ const SearchBar = () => {
 // Refactor from a function base component (above) to a class base component using ES6
 // Every class base component has a render() method
 // in a Class base component we need to use JSX otherwise it returns error
-
+// just class base component has state object
+// all JS classes have a special function call "constructor", it's the first and only function
+// automatically whenever a new instance of the class is created.
+// "super" : is the parent method ( here Component ) that we are calling that with probs
+// "state" is a plain JS object that exist on any class base component. the name of object property is arbitary.
+// Always manipulate "state" in the component with "this.setState"
 /* a class base component */
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={this.onInputChange}/>;
+    return <input onChange={ event => this.setState({ term: event.target.value})}/>;
     // for one line code we can also use ES6 synatx:
     //    return <input onChange={event => console.log(event.target.value)}/>;
   }
